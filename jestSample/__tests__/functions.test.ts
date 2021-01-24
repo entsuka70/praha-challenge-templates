dddd526fb91db3a6eb4fa51696b1b9ed9fc72a7e
entsuka70 <entsuka70@gmail.com>
2021-01-24 11:40:13 +0900

    feat: asyncSumOfArraySometimesZero カバレッジ100%

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -2,40 +2,69 @@
 
 import { sumOfArray } from "../functions";
 import { asyncSumOfArray } from "../functions";
+import { DatabaseMock } from "../util/index";
 import { asyncSumOfArraySometimesZero } from "../functions";
+import { doesNotMatch } from "assert";
 
-test('adds 1 + 1 + 1 to equal 3', () => {
-  expect(sumOfArray([1,1,1])).toBe(3);
+describe('Test sumOfArray', () => {
+    test('adds 1 + 1 + 1 to equal 3', () => {
+        expect(sumOfArray([1, 1, 1])).toBe(3);
+    });
+
+    test('adds 0 + 3 to equal 3', () => {
+        expect(sumOfArray([0, 3])).toBe(3);
+    });
+
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sumOfArray([1, 2])).toBe(3);
+    });
+
+    test('adds empty array', () => {
+        expect(() => sumOfArray([])).toThrow('Error : Exception !!');
+    });
+
+    test('adds String array', () => {
+        expect(() => sumOfArray(['1', 'Yes Error'])).toThrow('Error : Exception !!');
+    });
 });
 
-test('adds 0 + 3 to equal 3', () => {
-  expect(sumOfArray([0,3])).toBe(3);
+describe('Test asyncSumOfArray', () => {
+    test('async sum 1 + 1 + 1 to equal 3', () => {
+        return asyncSumOfArray([-1, 4]).then(data => {
+            expect(data).toEqual(3);
+        });
+    });
+
+    test('async sum 10 -7 to equal 3', () => {
+        return asyncSumOfArray([10, -7]).then(data => {
+            expect(data).toEqual(3);
+        });
+    });
 });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sumOfArray([1,2])).toBe(3);
-});
+describe('Test asyncSumOfArraySometimesZero', () => {
+    jest.mock('../util/index');
+    const database = jest.mock('../util/index');
 
-test('async sum 1 + 1 + 1 to equal 3', () => {
-  return asyncSumOfArray([-1,4]).then(data => {
-    expect(data).toEqual(3);
-  });
-});
-
-test('async sum 10 -7 to equal 3', () => {
-  return asyncSumOfArray([10, -7]).then(data => {
-    expect(data).toEqual(3);
-  });
-});
-
-test('async sum array sometimes zero', () => {
-  let number1:number = 1;
-  const mockDatabase = jest.fn();
-  mockDatabase.mockReturnValueOnce(2);
-
-  let insertNumber: number = number1 + mockDatabase();
-
-  return asyncSumOfArraySometimesZero([insertNumber]).then(data => {
-    expect(data).toEqual(3);
-  });
+    test('async sum of array', () => {
+        const myDatabaseMockSum = database.fn();
+        myDatabaseMockSum.mockImplementation(() => {
+            return {
+                save: (() => {
+                })
+            }
+        });
+        return expect(asyncSumOfArraySometimesZero([1,2,3], myDatabaseMockSum)).resolves.toEqual(6);
+    });
+    test('async sum of array to zero', () => {
+        const myDatabaseMockZero = database.fn();
+        myDatabaseMockZero.mockImplementation(() => {
+            return {
+                save: (() => {
+                        throw new Error ("fail!");
+                })
+            }
+        });
+        return expect(asyncSumOfArraySometimesZero([1,2], myDatabaseMockZero)).resolves.toBe(0);
+    });
 });
--- jestSample/functions.ts
+++ jestSample/functions.ts
@@ -2,44 +2,47 @@ import { NameApiService } from "./nameApiService";
 import { DatabaseMock } from "./util";
 
 export const sumOfArray = (numbers: number[]): number => {
-  if (Array.isArray(numbers) && numbers.length > 0) {
-    numbers.map(value => {
-      if (typeof value !== 'number') {
+    if (Array.isArray(numbers) && numbers.length > 0) {
+        numbers.map(value => {
+            if (typeof value !== 'number') {
+                throw new Error('Error : Exception !!');
+            }
+        });
+        return numbers.reduce((a: number, b: number): number => a + b);
+    } else {
         throw new Error('Error : Exception !!');
-      }
-    });
-    return numbers.reduce((a: number, b: number): number => a + b);
-  } else {
-    throw new Error('Error : Exception !!');
-  }
+    }
 };
 
 export const asyncSumOfArray = (numbers: number[]): Promise<number> => {
-  return new Promise((resolve): void => {
-    resolve(sumOfArray(numbers));
-  });
+    return new Promise((resolve): void => {
+        resolve(sumOfArray(numbers));
+    });
 };
 
 export const asyncSumOfArraySometimesZero = (
-  numbers: number[]
+    numbers: number[],
+    instance: any
 ): Promise<number> => {
-  return new Promise((resolve): void => {
-    try {
-      resolve(sumOfArray(numbers));
-    } catch (error) {
-      resolve(0);
-    }
-  });
+    return new Promise((resolve): void => {
+        try {
+            const database = new instance;
+            database.save();
+            resolve(sumOfArray(numbers));
+        } catch (error) {
+            resolve(0);
+        }
+    });
 };
 
-export const getFirstNameThrowIfLong = async (
-  maxNameLength: number
-): Promise<string> => {
-  const nameApiSerivce = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
-  const firstName = await nameApiSerivce.getFirstName();
+// export const getFirstNameThrowIfLong = async (
+//   maxNameLength: number
+// ): Promise<string> => {
+//   const nameApiSerivce = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
+//   const firstName = await nameApiSerivce.getFirstName();
 
-  if (firstName.length > maxNameLength) {
-    throw new Error("first_name too long");
-  }
-  return firstName;
-};
+//   if (firstName.length > maxNameLength) {
+//     throw new Error("first_name too long");
+//   }
+//   return firstName;
+// };
